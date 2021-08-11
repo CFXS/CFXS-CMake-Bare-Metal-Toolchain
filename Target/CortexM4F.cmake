1bfd35483dd9509c6dc7467d8006546ad215e907
CFXS <rihardsveips@gmail.com>
2021-08-12 01:20:07 +0300

    target bkpt define

--- Target/CortexM4F.cmake
+++ Target/CortexM4F.cmake
@@ -2,5 +2,6 @@ function(Target_CortexM4F)
 
 set(CPU_OPTIONS ${CPU_OPTIONS} -mthumb -mcpu=cortex-m4 -march=armv7e-m -mfpu=fpv4-sp-d16 -mfloat-abi=hard PARENT_SCOPE)
 add_compile_definitions("CORE_CORTEX_M4")
+add_compile_definitions("__bkpt=asm volatile(\"bkpt 1\")")
 
 endfunction()
