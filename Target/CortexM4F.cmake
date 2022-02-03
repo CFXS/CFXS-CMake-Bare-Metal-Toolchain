0ca77c59d6dc11f4b20b7bf60d89cad3f10f4991
RVeips <rihardsveips@gmail.com>
2022-02-03 21:56:57 +0200

    cortex m4 tune

--- Target/CortexM4F.cmake
+++ Target/CortexM4F.cmake
@@ -1,6 +1,6 @@
 function(Target_CortexM4F)
 
-set(CPU_OPTIONS ${CPU_OPTIONS} -mthumb -mcpu=cortex-m4 -march=armv7e-m -mfpu=fpv4-sp-d16 -mfloat-abi=hard PARENT_SCOPE)
+set(CPU_OPTIONS ${CPU_OPTIONS} -mthumb -mcpu=cortex-m4 -march=armv7e-m -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mtune=cortex-m4 PARENT_SCOPE)
 add_compile_definitions("CFXS_CORE_CORTEX_M")
 add_compile_definitions("CFXS_CORE_CORTEX_M4")
 add_compile_definitions("CFXS_CORE_HAS_FPU")
