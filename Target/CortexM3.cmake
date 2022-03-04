4246fbdca6634b29db9f504a145b8c3207bd1c4c
RVeips <rihardsveips@gmail.com>
2022-03-05 01:05:58 +0200

    m3

---
+++ Target/CortexM3.cmake
@@ -0,0 +1,8 @@
+function(Target_CortexM4F)
+
+set(CPU_OPTIONS ${CPU_OPTIONS} -mthumb -mcpu=cortex-m3 -march=armv7e-m -mtune=cortex-m3 PARENT_SCOPE)
+add_compile_definitions("CFXS_CORE_CORTEX_M")
+add_compile_definitions("CFXS_CORE_CORTEX_M3")
+add_compile_definitions("__bkpt=asm volatile(\"bkpt 0\")")
+
+endfunction()
