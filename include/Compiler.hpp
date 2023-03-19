4c08bbb98cfee85d7c409c7cf4349b00cdd435e4
RVeips <rihardsv@na.lv>
2023-03-20 00:45:42 +0200

    precomp
    Signed-off-by: RVeips <rihardsv@na.lv>

---
+++ include/Compiler.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#define __interrupt      __attribute__((interrupt("irq")))
+#define __weak           __attribute__((weak))
+#define __used           __attribute__((used))
+#define __noinit         __attribute__((section(".noinit")))
+#define __vector_table   __attribute__((section(".vector_table"), used))
+#define __naked          __attribute__((naked))
+#define __noreturn       __attribute__((noreturn))
+#define __noinit         __attribute__((section(".noinit")))
+#define __memory_barrier asm volatile("" ::: "memory")
+#define __rw             volatile
+#define __ro             const volatile
+
+#define __mem8(x)    (*(__rw uint8_t*)(x))
+#define __mem16(x)   (*(__rw uint16_t*)(x))
+#define __mem32(x)   (*(__rw uint32_t*)(x))
+#define __mem64(x)   (*(__rw uint64_t*)(x))
+#define __memT(T, x) (*(__rw T*)(x))
